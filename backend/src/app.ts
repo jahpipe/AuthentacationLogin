432e045a41a109e0dd40fdd54261424627cb43a5
unknown <jovaniemoreno420@gmail.com>
2025-08-04 16:33:01 +0800

    updated the cors problem

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,9 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
 app.use(cors({
-  origin: "http://localhost:5173",  // ✅ for dev
+  origin: "https://authentacation-login.vercel.app", // ✅ include https://
   credentials: true,
 }));
+
 
 //Router//
 app.use('/api', accountRoutes)
